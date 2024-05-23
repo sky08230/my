a07b1a3d0b29849c20777007baf45739e3088608
Shiyi <sgong@pandemonium.dev>
2024-05-22 22:30:46 -0500

    update cashback

--- banking_system_imp.py
+++ banking_system_imp.py
@@ -1,4 +1,5 @@
 import logging
+from collections import defaultdict
 
 # set up the logger file 
 logging.basicConfig(filename='log_bank.csv',filemode='w+',format='%(asctime)s -%(name)s -%(levelname)s -%(message)s')
@@ -6,63 +7,73 @@ logger=logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-class BankingSystemImp:
-    """
-    create banksystem class to perform functions create account, withdraw, deposit and transfer.
-    """
-    #initialize the class with account data dictionary
+
+class BankingSystemImpl:
+
     def __init__(self):
-        self.account={}
-
-    # function to create account    
-    def create_account(self, name, balance=0):
-        if name in self.account.keys():
-            logger.error("%s already exist",name)
-            return False
-         
-        else:
-            self.account[name]=balance
-            logger.info("successfully create account %s with %s", name, balance)
-            return self.account[name]
-
-     # function to deposit money for account        
-    def deposit(self, name, amount):
-        if name not in self.account.keys():
-            logger.error("%s not exist in the system",name)
-            return False
-        else:
-            self.account[name]=amount+self.account[name]
-            logger.info("successfully deposit account %s with %s", name, amount)
-            return self.account[name]
+        self.account=[]
+        self.balance={}
+        self.transf=defaultdict(lambda :0)
+        self.cashback=[]
+        self.payid=0
         
-    # function to witdraw money for account        
-    def withdraw(self, name, amount):
-        if name not in self.account.keys():
-            logger.error("%s not exist in the system",name)
-            return False
-        #check if the money is enough 
-        elif self.account[name]< amount:
-            logger.error("overdraft account")
-            return False       
-        else:
-             self.account[name]=self.account[name]-amount
-             logger.info("successfully withdraw account %s with %s", name, amount)
-             return self.account[name]
+    def cashback(fun):
+        def call(self,*args):
+            for item in self.cashback:
+                if self.cashback!=[] and item[0]<=args[0]:
+                    self.cashback.remove(item)
+                    self.deposit(args[0],item[1],item[2])
+            return fun(self,*args)                        
+        return call
+                          
         
-    # function to transfer money for account              
-    def transfer(self, source_account, target_account, amount):
-        if source_account not in self.account.keys() or target_account not in self.account.keys():
-            logger.error("account not exist in the system")
+    @cashback    
+    def create_account(self, timestamp: int, account_id: str):
+        if account_id in self.account:
             return False
-        elif self.account[source_account]<amount:
-            logger.error("overdraft account")
-            return False
+        else:
+            self.account.append(account_id)  
+            self.balance[account_id]=0
+            self.transf[account_id]=0
+            return True
+        
+    @cashback    
+    def deposit(self, timestamp: int, account_id: str, amount: int):
+        if account_id not in self.account:
+            return None
+        else:
+            self.balance[account_id]=amount+self.balance[account_id]
+            return self.balance[account_id]
+    @cashback        
+    def transfer(self, timestamp: int, source_account_id: str, target_account_id: str, amount: int):
+        if source_account_id not in self.account or target_account_id not in self.account:
+            return None
+        elif  source_account_id== target_account_id:
+            return None
+        elif self.balance[source_account_id]<amount:
+            return None
         else:  
-            self.account[source_account]=self.account[source_account]-amount
-            self.account[target_account]=self.account[target_account]+amount
-            logger.info("successfully transfer from %s to %s with %s", source_account,target_account, amount)
-            return self.account[source_account],self.account[target_account]
-
+            self.transf[source_account_id]=self.transf[source_account_id]+amount
+            self.balance[source_account_id]=self.balance[source_account_id]-amount
+            self.balance[target_account_id]=self.balance[target_account_id]+amount
+            return self.balance[source_account_id]
+    @cashback
+    def top_spenders(self, timestamp: int, n: int):
+        rank=sorted(self.account,key=lambda k:self.transf[k],reverse=True)
+        return rank[:n]
+        
+    @cashback
+    def pay(self, timestamp: int, account_id: str, amount: int):
+        if account_id not in self.account:
+            return None
+        elif self.balance[account_id]< amount:
+            return None
+        else:
+            self.balance[account_id]=self.balance[account_id]-amount
+            self.transf[account_id]=self.transf[account_id]+amount
+            self.cashback.append([timestamp+86400000,account_id,0.02*amount])
+            self.payid+=1
+            return 'payment'+str({self.payid})+'succesfully'
 
      
 
